1558915826bb982b3b31b5ba70f588fa16fd52dd
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-24 01:12:17 +0200

    Added buildControls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './BuildControls.module.css'
+const buildControls = (props) => (
+    <div className={style.BuildControls}>
+
+    </div>
+);
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import style from './Burger.module.css';
 import BurgerIngredient from "./Ingredients/BurgerIngredient";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey =>
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey =>
         [...Array(props.ingredients[igKey])].map((_, i) =>
            <BurgerIngredient key={igKey + i} type={igKey}/> // [,]
         )
-    );
+    ).reduce((arr,el) => {
+        return arr.concat(el);
+    }, []);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     console.log(transformedIngredients);
     return(
         <div className={style.Burger}>
