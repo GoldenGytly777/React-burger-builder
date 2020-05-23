f1a9331501e6ed8a8e3ab4cdd759715de5185bbd
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-24 00:08:11 +0200

    Added dynamic ingredients component

---
+++ src/components/Burger/Ingredients/BurgerIngredient.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import style from './BurgerIngredients.module.css';
+
+const burgerIngredient= (props) => {
+    let ingredient = null;
+
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingredient = <div className={style.BreadBottom}></div>;
+            break;
+        case('bread-top'):
+            ingredient = (
+                <div className={style.BreadTop}>
+                    <div className={style.Seeds1}></div>
+                    <div className={style.Seeds2}></div>
+                </div>
+                );
+            break;
+        case('meat'):
+            ingredient = <div className={style.Meat}></div>;
+            break;
+        case('Cheese'):
+            ingredient = <div className={style.Cheese}></div>;
+            break;
+        case('Salad'):
+            ingredient = <div className={style.Salad}></div>;
+            break;
+        case('Bacon'):
+            ingredient = <div className={style.Bacon}></div>;
+            break;
+
+    };
+
+export default burgerIngredient;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, {Fragment} from 'react';
-
+import style from './Layout.module.css';
 
 const layout = (props) => (
     <Fragment>
         <div>
             Toolbar, SideDrawer, Backdrop
         </div>
-        <main>
+        <main className={style.Content}>
             {props.children}
         </main>
     </Fragment>
