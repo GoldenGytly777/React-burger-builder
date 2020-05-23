ae5ea22ba938c4254873c6996c99bb12e49c365a
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-23 22:13:09 +0200

    Added better file management

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends Component {
+  render(){
+    return (
+        <div >
+
+        </div>
+    );
+  }
+
+
 }
 
 export default App;
